43bedaf08dc83b2ba66569ad1cfc4061fb480eb4
yewon <shimyewon@ewhain.net>
2023-09-16 15:41:22 +0900

    aws_project

---
+++ post/admin.py
@@ -0,0 +1,8 @@
+
+
+# Register your models here.
+from django.contrib import admin
+from post.models import Post
+
+# Register your models here.
+admin.site.register(Post)
---
+++ post/views.py
@@ -0,0 +1,18 @@
+
+
+# Create your views here.
+from .serializers import *
+
+from rest_framework import viewsets, filters
+
+# Create your views here.
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset=Post.objects.all()
+    serializer_class=PostSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title']
+
+
+    def perform_create(self, serializer):
+        serializer.save()
